679439c57be7c9ec3a56b338089824d40d476a13
Tomas Tasselli <tomastasselli@gmail.com>
2025-09-16 17:56:28 -0300

    1ra version de la poc

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,39 +1,87 @@
-import preactLogo from '../../assets/preact.svg';
 import './style.css';
 
 export function Home() {
 	return (
 		<div class="home">
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
+			<a href="https://www.todoshowcase.com/cartelera/rosario" target="_blank">
+				<img src="https://www.todoshowcase.com/themes/default/images/common/logo-showcase.svg" alt="Shoowcase logo" height="50" width="500" />
 			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
+			<h1>Cartelera de películas </h1>
 			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
+				<Gallery 
+					title="Kimetsu no Yaiba: Castillo infinito"
+					description="Película de anime"
+					src="https://static.voyalcine.net/Uploads/i4766.jpg"
 				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
+				<Gallery
+					title="200% Lobo"
+					description="Película de animación"
+					src="https://static.voyalcine.net/Uploads/i4854.jpg"
 				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
+				<Gallery
+					title="Together: Juntos hasta la muerte"
+					description="Película de terror"
+					src="https://static.voyalcine.net/Uploads/i4853.jpg"
+				/>
+				<Gallery
+					title="La mujer de la fila"
+					description="Película de suspenso"
+					src="https://static.voyalcine.net/Uploads/i4843.jpg"
+				/>
+				<Gallery
+					title="El conjuro 4: últimos ritos"
+					description="Película de terror"
+					src="https://static.voyalcine.net/Uploads/i4800.jpg"
+				/>
+				<Gallery
+					title="Mascotas al rescate"
+					description="Película de animación"
+					src="https://static.voyalcine.net/Uploads/i4821.jpg"
+				/>
+				<Gallery
+					title="Homo Argentum"
+					description="Película de Francella"
+					src="https://static.voyalcine.net/Uploads/i4732.jpg"
+				/>
+				<Gallery
+					title="Otro viernes de locos"
+					description="Película de Comedia"
+					src="https://static.voyalcine.net/Uploads/i4748.jpg"
+				/>
+				<Gallery
+					title="La hora de la desaparición"
+					description="Película de Terror"
+					src="https://static.voyalcine.net/Uploads/i4733.jpg"
+				/>
+				<Gallery
+					title="Los cuatro Fantásticos: primeros pasos"
+					description="Película de Marvel"
+					src="https://static.voyalcine.net/Uploads/i4682.jpg"
+				/>
+				<Gallery
+					title="Los tipos malos 2"
+					description="Película de Animación"
+					src="https://static.voyalcine.net/Uploads/i4711.jpg"
+				/>
+				<Gallery
+					title="Toy Story: 30 aniversario"
+					description="Película de Animación"
+					src="https://static.voyalcine.net/Uploads/i4850.jpg"
 				/>
 			</section>
 		</div>
 	);
 }
 
-function Resource(props) {
+function Gallery(props) {
 	return (
-		<a href={props.href} target="_blank" class="resource">
+		<div class="gallery">
+			<img src={props.src}
+				alt={props.title}
+				height="auto" 
+				width="100%" />
 			<h2>{props.title}</h2>
 			<p>{props.description}</p>
-		</a>
+		</div>
 	);
 }
